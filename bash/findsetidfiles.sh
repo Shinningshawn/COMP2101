71f0db2ccb694146909d9d339cc11d40a7c033a9
Shinningshawn <shawn.violette@mygeorgian.ca>
2019-09-20 11:17:31 -0700

    Findsetidfiles Finished V2 200407956

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -17,13 +17,17 @@ find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
 #the 2000 searches for special permissions on files in the group set, with 4000 being special permissions for owner set and 6000 would search only for ones with for both
 #the -ls 2>/dev/null says to take all responses with exit code 2 (in this case for invalid permissions) and sent to /dev/null which discards the text
 #lastly the output is piped into sort with -k 3 sorting it by the 3rd key which means the owner name
+
 echo "setgid files"
 echo "============="
 find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 3
 
-#ls to list all files and directories. -lh for long list and human readable values (g,m,k) and capital S to sort by file sizes
-#the output is piped into head -11 to show the first 11 lines and piped into tail -10 for only last 10 lines
-#(There's probably a better way to get rid of the one line that shows total storage of all files, but that's the best i came up with)
+#find to search for files, / indicating to search the whole system, 2>/dev/null to void all permission errors
+#-exec executes the following commands on a return status of 0. The {} indicates where the files from the earlier segment are appended and the + to indicate where the -exec finishes
+#ls lists the files recieved with -l outputting long list and --block-size=M telling the system to format the size into Mbits.
+#the output is piped into sort -hr to sort the files in readable number order and r for reverse order (sorts smallest first otherwise)
+# lastly head to show the first 10 files in the list
+
 echo "10 largest files on system"
 echo "============="
-ls -lhS / | head -11 | tail -10
+find / 2>/dev/null -exec ls -l --block-size=M {} + | sort -hr -k 5 | head -10
