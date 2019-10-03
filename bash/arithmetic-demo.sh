ff369a7332589866555ac43509d3049edb08ed7b
Shinningshawn <shawn.violette@mygeorgian.ca>
2019-10-03 11:17:57 -0700

    Lab 2 arithmetic-demo completted 200407956

---
+++ bash/arithmetic-demo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# this script demonstrates doing arithmetic
+
+# Improve this script by asking the user to supply the two numbers
+# Improve this script by demonstrating using subtraction and multiplication
+# Improve this script by demonstrating the modulus operation
+#   - the output should look something like:
+#   - first divided by second gives X with a remainder of Y
+# Improve this script by calculating and displaying the first number raised to the power of the second number
+
+
+#read with -p to provide a prompt to capture text, some text to explain what the user is being asked for.
+#the input is assigned assigned to the variables called firstnum and secondnum
+read -p "input a number :" firstnum
+read -p "input a second number :" secondnum
+
+# a collection of lines performing various calculations using different arithmetic signs
+# + is addition, - is subtractions, / is division (without any remainder), % is modulus (division's remainder), ** is exponent
+sum=$((firstnum + secondnum))
+difference=$((firstnum - secondnum))
+dividend=$((firstnum / secondnum))
+product=$((firstnum * secondnum))
+modulus=$((firstnum % secondnum))
+powerOf=$((firstnum ** secondnum))
+#
+fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnum/$secondnum}")
+
+#each line outputs the assigned variables explaining what math was performed for each result
+cat <<EOF
+
+$firstnum plus $secondnum is $sum
+$firstnum subtract $secondnum is $difference
+$firstnum multiplied by $secondnum is $product
+$firstnum modulus $secondnum is $modulus
+$firstnum raised to the power of $secondnum is $powerOf
+$firstnum divided by $secondnum is $dividend with a remainder of $modulus
+  - More precisely, it is $fpdividend
+EOF
