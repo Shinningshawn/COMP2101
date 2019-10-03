4a57a3d34e6d82114a7ab8226f48c28a3d395154
Shinningshawn <shawn.violette@mygeorgian.ca>
2019-10-03 12:07:28 -0700

    welcome-message.sh completted 200407956

---
+++ bash/welcome-message.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# This script produces the dynamic welcome message
+# it should look like
+#   Welcome to planet hostname, title name!
+
+# Improve this script by using the value in the automatic variable $USER for the name
+# Improve this script by adding some time and day of the week information to the welcome message
+#   Use a format like this:
+#   It is HH:MM AM on weekday.
+
+###############
+# Variables   #
+###############
+
+title="Overlord"
+myname="dennis"
+hostname=$USER
+CurrentTime=$(date +%H:%M:%S)
+currentDate=$(date +%A)
+
+###############
+# Main        #
+###############
+cat <<EOF
+
+Welcome to planet $hostname, "$title $myname!"
+
+The current time is $CurrentTime on a $currentDate
+EOF
