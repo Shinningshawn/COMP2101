3ac22ea586272cd8dc15b7d3e8cae7f4bdae5755
Shinningshawn <shawn.violette@mygeorgian.ca>
2019-10-10 08:25:31 -0700

    Lab 3 login-welcome completted 200407956

--- bash/login-welcome.sh
+++ bash/login-welcome.sh
@@ -23,9 +23,13 @@ currentDate=$(date +%A)
 # Main        #
 ###############
 
-
+#difference from the welcome message script, we instead save the lines to variables...
+#the 'error' messages seem to only care about the quotes used around the $title $myname
+#i can remove them but they'll not be present around the 'overlord dennis' part in output
+#however the script runs w/o issue with or without
 welcome1="Welcome to planet $hostname, '$title $myname!'"
 timedate="The current time is $CurrentTime on a $currentDate"
 
+#and then we echo them and pipe them through cowsay
 echo $welcome1 | cowsay
 echo $timedate | cowsay
